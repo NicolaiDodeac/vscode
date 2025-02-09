ef19ef92b5864e9d7a73ab1a5abf6d00c47e815e
Nicolai Dodeac <Mykola.Dodiak@gmail.com>
2025-02-09 11:56:14 +0000

    numpy

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("Hello world")
-print("this is my first project")
+# print("Hello world")
+# print("this is my first project")
+# import numpy as np
+
+# a = np.array([1, 2, 3, 4, 5], dtype=float)
+# print(a)
+
+# m = np.array([[1, 2, 3], [4, 5, 6]], dtype=int)
+# print(m)
+import numpy as np
+a = np.array([1, 2, 3, 4, 5], dtype=float);print(a.shape)  
+m = np.array([[1, 2, 3], [4, 5, 6]], dtype=int);
+print(m.shape)
