bfaa6c2fa95c78e5b01ab0cc7a10cb813938dae1
Nicolai Dodeac <Mykola.Dodiak@gmail.com>
2025-02-10 18:09:24 +0000

    types

--- main.py
+++ main.py
@@ -7,7 +7,23 @@
 
 # m = np.array([[1, 2, 3], [4, 5, 6]], dtype=int)
 # print(m)
+# import numpy as np
+# a = np.array([1, 2, 3, 4, 5], dtype=float);print(a.shape)  
+# m = np.array([[1, 2, 3], [4, 5,3], [4, 5,3]], dtype=int);
+# print(m.shape)
+
+# import numpy as np
+
+# a = np.ones((5,), dtype=float)
+# print(a)
+
+# m = np.ones((2, 3), dtype=int)
+# print(m)
+
 import numpy as np
-a = np.array([1, 2, 3, 4, 5], dtype=float);print(a.shape)  
-m = np.array([[1, 2, 3], [4, 5, 6]], dtype=int);
-print(m.shape)
+
+a = np.arange(5)
+b = np.arange(1, 3, 0.5)
+
+print(a)
+print(b)
